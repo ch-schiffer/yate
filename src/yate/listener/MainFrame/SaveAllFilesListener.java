22dca2565f89cfd5142b1ec50a28965bb3c29d99
Carpae <Carpae@Carina>
2014-12-19 14:16:41 +0100

    in SaveAllFilesListener

--- src/yate/listener/MainFrame/SaveAllFilesListener.java
+++ src/yate/listener/MainFrame/SaveAllFilesListener.java
@@ -2,8 +2,10 @@ package yate.listener.MainFrame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFileChooser;
 import yate.managers.FileManager;
 import yate.model.MainFrameModel;
+import yate.project.File;
 import yate.view.MainFrameView;
 
 /**
@@ -11,14 +13,27 @@ import yate.view.MainFrameView;
  * @author Laurin
  */
 public class SaveAllFilesListener extends MainFrameListener implements ActionListener {
-
+    
     public SaveAllFilesListener(MainFrameView view, MainFrameModel model) {
         super(view, model);
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
+        for (File file: FileManager.getInstance().getAllFiles()) {
+            if (!file.isValid()){
+                JFileChooser openFile = new JFileChooser();
+                if (openFile.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    file.setPath(openFile.getSelectedFile().getPath());
+                    file.setContent(model.getCurrentCenterBox().getText());
+                    view.setCurrentTabTitle(file.getName());
+                    
+               //     FileManager.getInstance().setCurrentFile(file);
+              //      FileManager.getInstance().saveCurrentFile();
+                }
+            }  
+        }
         FileManager.getInstance().saveAllFiles();
     }
-
+    
 }
--- src/yate/managers/FileManager.java
+++ src/yate/managers/FileManager.java
@@ -123,6 +123,8 @@ public class FileManager {
         this.currentFile = currentFile;
     }
     
-    
+    public List<File> getAllFiles() {
+        return allFiles;
+    }
     
 }
